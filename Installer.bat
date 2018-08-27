35abb097d0ca00211dcdb01e1b86307c8f090492
Unknown <krokofant@users.noreply.github.com>
2018-08-27 16:15:10 +0200

    Create temp folder

--- Installer.bat
+++ Installer.bat
@@ -1,4 +1,5 @@
 REM Download latest installer
+mkdir temp
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& wget 'https://raw.githubusercontent.com/krokofant/lovely-powershell/master/Installer.ps1' -OutFile .\temp\Installer.ps1"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& .\temp\Installer.ps1"
 
