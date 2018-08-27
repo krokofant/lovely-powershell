f8e27a0695d17a8961c76e25ba5e2edae5cce2a5
Unknown <krokofant@users.noreply.github.com>
2018-08-27 16:07:12 +0200

    Update

--- LovelyShell.psm1
+++ LovelyShell.psm1
@@ -1,16 +1,14 @@
 function installIfMissing {
-    param
-      (
-          [string] $ModuleName
-      )
-      if (!(Get-Module -ListAvailable -Name $ModuleName)) {
-        Write-Output "Installing missing module $ModuleName"
-        Install-Module -Name $ModuleName -Scope CurrentUser
-      }
+  param
+    (
+        [string] $ModuleName
+    )
+    if (!(Get-Module -ListAvailable -Name $ModuleName)) {
+      Write-Output "Installing missing module $ModuleName"
+      Install-Module -Name $ModuleName -Scope CurrentUser
+    }
 }
 
-# Hide username from prompt prefix
-$DefaultUser = $env:USERNAME
 $PrimaryTheme = "agnoster"
 $SecondaryTheme = "sorin"
 
@@ -19,12 +17,16 @@ installIfMissing posh-git
 installIfMissing oh-my-posh
 installIfMissing Get-ChildItemColor
 
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module Get-ChildItemColor
+
 # Fix a oh-my-posh color issue
 Set-PSReadlineOption -ResetTokenColors
 
 # Prettier ls/l output
-Set-Alias l Get-ChildItemColor -Option AllScope
-Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
+Set-Alias -Scope Global l Get-ChildItemColor -Option AllScope
+Set-Alias -Scope Global ls Get-ChildItemColorFormatWide -Option AllScope
 
 # Path shortcuts
 Set-Alias ~ cdhome -Option AllScope
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,5 @@
 # Load LovelyShell
 Import-Module $PSScriptRoot\LovelyShell.psm1
+
+# Hide username from prompt prefix
+$DefaultUser = $env:USERNAME
