15575124e770cd88a3a21afeaf7ec762cf492780
Unknown <krokofant@users.noreply.github.com>
2018-08-27 16:49:23 +0200

    Try to work around tls/ssl issue

--- Installer.bat
+++ Installer.bat
@@ -1,6 +1,6 @@
 REM Download latest installer
 mkdir temp
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& wget 'https://raw.githubusercontent.com/krokofant/lovely-powershell/master/Installer.ps1' -OutFile .\temp\Installer.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; wget 'https://raw.githubusercontent.com/krokofant/lovely-powershell/master/Installer.ps1' -OutFile .\temp\Installer.ps1"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& .\temp\Installer.ps1"
 
 REM PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& .\Installer.ps1"
