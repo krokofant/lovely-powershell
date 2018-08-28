1d097cdcc01699bce9207b6bb2aa1e9c41d52ec0
Unknown <krokofant@users.noreply.github.com>
2018-08-28 08:56:22 +0200

    Test another download method

--- Installer.bat
+++ Installer.bat
@@ -1,6 +1,6 @@
 REM Download latest installer
 mkdir temp
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls, Ssl3'; wget 'https://raw.githubusercontent.com/krokofant/lovely-powershell/master/Installer.ps1' -OutFile .\temp\Installer.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/krokofant/lovely-powershell/master/Installer.ps1', '.\temp\Installer.ps1')"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& .\temp\Installer.ps1"
 
 REM PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& .\Installer.ps1"
