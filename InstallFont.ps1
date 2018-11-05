60722f70d58172a540f915ecf725ee5518ac88b0
Krokofant <krokofant@users.noreply.github.com>
2018-11-05 22:39:33 +0100

    Fix it again

--- InstallFont.ps1
+++ InstallFont.ps1
@@ -4,7 +4,7 @@ $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
 
 # Install powerline fonts
 New-Item lp-temp -ItemType Directory
-wget -Headers @{"Cache-Control"="no-cache"} 'https://github.com/powerline/fonts/blob/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf?raw=true' -OutFile .\temp\DejaVuSansMonoPowerLine.ttf
+wget -Headers @{"Cache-Control"="no-cache"} 'https://github.com/powerline/fonts/blob/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf?raw=true' -OutFile .\lp-temp\DejaVuSansMonoPowerLine.ttf
 $FONTS = 0x14
 $objShell = New-Object -ComObject Shell.Application
 $objFolder = $objShell.Namespace($FONTS)
